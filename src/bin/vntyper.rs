3a04a314971b6a2c2c3af3b19e699fa3e59e0eae
Do Duy <juchiast@gmail.com>
2016-11-07 11:50:57 +0700

    set up front-end

--- src/bin/vntyper.rs
+++ src/bin/vntyper.rs
@@ -3,9 +3,47 @@
 // <http://opensource.org/licenses/MIT>
 
 extern crate gtk;
+extern crate vntyper;
+
+use vntyper::input;
+use vntyper::input_method;
+
 use gtk::prelude::*;
 use gtk::Window;
 use gtk::TextView;
+use gtk::TextBuffer;
+use gtk::TextIter;
+
+trait Buffer {
+    fn get_insert_iter(&self) -> Option<TextIter>;
+    fn complete(&self, c: char) -> Inhibit ;
+}
+
+macro_rules! gtk_try {
+    ( $x:expr ) => {
+        {
+            let tmp = $x;
+            if $x.is_none() { return Inhibit(false) }
+            tmp.unwrap()
+        }
+    };
+}
+
+impl Buffer for TextBuffer {
+    fn get_insert_iter(&self) -> Option<TextIter> {
+        self.get_insert().map(|x| self.get_iter_at_mark(&x))
+    }
+    fn complete(&self, c: char) -> Inhibit {
+        let end_iter = gtk_try!(self.get_insert_iter());
+
+        let mut start_iter = end_iter.clone();
+        start_iter.backward_chars(15);
+
+        let text = gtk_try!(self.get_text(&start_iter, &end_iter, false));
+        println!("{}", text);
+        Inhibit(false)
+    }
+}
 
 fn main() {
     if gtk::init().is_err() {
@@ -18,9 +56,14 @@ fn main() {
     window.set_title("Vietnamese Input");
     window.show_all();
 
-    text_view.connect_key_press_event(|_, ev| {
-        println!("{}", ev.get_keyval() as u8 as char);
-        Inhibit(false)
+    text_view.connect_key_press_event(|widget, ev| {
+        match std::char::from_u32(ev.get_keyval()) {
+            Some(key) => match widget.get_buffer() {
+                Some(buffer) => buffer.complete(key),
+                None => Inhibit(false),
+            },
+            None => Inhibit(false),
+        }
     });
 
     window.connect_delete_event(|_, _| {
